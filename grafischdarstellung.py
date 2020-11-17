0f7a4dd6225d95bc8dc39555eb7d2ddf768f5ef3
Chantalkle <33465110+Chantalkle@users.noreply.github.com>
2020-11-17 16:21:34 +0100

    Create grafischdarstellung.py

---
+++ grafischdarstellung.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+# die X-Werte:
+# Satzlh =[] 
+# für jede Satzlänge elemente für alle vorherigen hinzu füge, oder wenn schon gibt, dann +1
+satzlaenge = list(range(0, len(Satzlh)))
+print(days)
+# die Y-Werte:
+haeufigkeit = satzlh
+plt.plot(satzlaenge, haeufigkeit)
+plt.show()
